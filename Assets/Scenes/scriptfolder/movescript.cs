fb54e099e0ec7bf094546d3b4cc6c39164634a9d
Bill <l.m.z6677hotmail@gmail.com>
2022-11-02 01:08:07 -0400

    debuged move

--- Assets/Scenes/scriptfolder/movescript.cs
+++ Assets/Scenes/scriptfolder/movescript.cs
@@ -18,20 +18,25 @@ public class movescript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (script.moveLimit !=0)
-        {
+        
+        
             transform.position= Vector3.MoveTowards(transform.position,movePoint.position,moveSpeed* Time.deltaTime);
+            
             if(Vector3.Distance(transform.position,movePoint.position)<=.05f){
                 if(Mathf.Abs(Input.GetAxisRaw("Horizontal"))==1f){
-                    if(!Physics2D.OverlapCircle(movePoint.position+new Vector3(Input.GetAxisRaw("Horizontal"),0f,0f),.01f,stopmovement))
-                        {movePoint.position+=new Vector3(Input.GetAxisRaw("Horizontal"),0f,0f);script.moveLimit -=1;}
+                    if(!Physics2D.OverlapCircle(movePoint.position+new Vector3(Input.GetAxisRaw("Horizontal"),0f,0f),.01f,stopmovement)){
+                        if (script.moveLimit !=0)
+                            {movePoint.position+=new Vector3(Input.GetAxisRaw("Horizontal"),0f,0f);script.moveLimit -=1;}
+                    }                          
                 }
                 else if(Mathf.Abs(Input.GetAxisRaw("Vertical"))==1f){
-                    if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f,Input.GetAxisRaw("Vertical"), 0f), .01f, stopmovement))
-                        { movePoint.position += new Vector3(0f,Input.GetAxisRaw("Vertical"), 0f); script.moveLimit -=1;}
+                    if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f,Input.GetAxisRaw("Vertical"), 0f), .01f, stopmovement)){
+                        if (script.moveLimit !=0)
+                            { movePoint.position += new Vector3(0f,Input.GetAxisRaw("Vertical"), 0f); script.moveLimit -=1;}
+                    }                      
                 }
             }
-        }
+        
         
 
         
